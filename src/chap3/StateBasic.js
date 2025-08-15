2373e52c2c46f5715d546c7a409b37906a772fff
norman6464 <resjimkalto89890@emaiol.com>
2025-08-15 16:17:06 +0900

    chap3 StateBasiuc

---
+++ src/chap3/StateBasic.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+// { init }はPropsの分割代入
+export default function StateBasic({ init }) {
+  // Props（init）でState（count）を初期化
+  const [count, setCount] = useState(init);
+
+  // カウンタボタンクリック時にState値をインクリメントをする
+  const handleClick = () => {
+    setCount(() => count + 1);
+  };
+  return (
+    <>
+      <button onClick={handleClick}>カウント</button>
+      <p>{count}回クリックされました。</p>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import EventBasic from './chap3/EventBasic ';
+import StateBasic from './chap3/StateBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<EventBasic type="time" />);
+root.render(<StateBasic init={0} />);
